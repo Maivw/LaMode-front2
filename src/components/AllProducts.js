6df946608ab155111ac1779493a34285ed810f48
tran <fidtran@gmail.com>
2020-07-08 19:15:02 -0400

    config material ui

--- src/components/AllProducts.js
+++ src/components/AllProducts.js
@@ -3,6 +3,56 @@ import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../reducers/productManagement";
 import "../index.css";
 import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		display: "flex",
+		flexWrap: "wrap",
+		justifyContent: "space-around",
+		overflow: "hidden",
+		backgroundColor: theme.palette.background.paper,
+	},
+	gridList: {
+		width: 1100,
+		height: "auto",
+	},
+	icon: {
+		color: "rgba(255, 255, 255, 0.54)",
+	},
+	productImgs: {
+		width: 300,
+		height: "auto",
+		borderRadius: "10px",
+	},
+	addToCartButton: {
+		margin: "20px",
+		borderRadius: "10px",
+		backgroundColor: "#1b5e20",
+		color: "white",
+		fontWeight: "400",
+		paddingTop: "5px",
+		paddingBottom: "5px",
+		paddingLeft: "10px",
+		paddingRight: "10px",
+		display: "flex",
+		justifyContent: "cener",
+	},
+	customGlTitleBar: {
+		backgroundColor: "white",
+		display: "flex",
+		justifyContent: "center",
+		alignItem: "cener",
+		marginRight: "60px",
+	},
+	link: {
+		textDecoration: "none",
+		color: "#07ad90",
+		fontSize: "14px",
+	},
+}));
 
 export default function AllProducts(props) {
 	const products = useSelector((state) => state.productManagement.products);
