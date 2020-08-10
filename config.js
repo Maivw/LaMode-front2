8f63a453158e23a400911d3164048b9af2cb0dde
tran <fidtran@gmail.com>
2020-08-10 19:28:16 -0400

    deployment

--- config.js
+++ config.js
@@ -9,5 +9,5 @@ module.exports = {
 	apiBaseUrl:
 		process.env.NODE_ENV == "development"
 			? process.env.REACT_APP_API_BASE_URL
-			: "https://git.heroku.com/lamodebackend2.git",
+			: "https://lamodebackend2.herokuapp.com/",
 };
