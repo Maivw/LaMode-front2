cbc33c90e3dd468460fd86621ac8eb7afb6da94d
tran <fidtran@gmail.com>
2020-08-10 19:21:40 -0400

    deployment

--- config.js
+++ config.js
@@ -1,6 +1,13 @@
+// module.exports = {
+// 	apiBaseUrl:
+// 		process.env.NODE_ENV === "development"
+// 			? process.env.REACT_APP_API_BASE_URL
+// 			: "",
+// };
+
 module.exports = {
 	apiBaseUrl:
-		process.env.NODE_ENV === "development"
+		process.env.NODE_ENV == "development"
 			? process.env.REACT_APP_API_BASE_URL
-			: "",
+			: "https://git.heroku.com/lamodebackend2.git",
 };
