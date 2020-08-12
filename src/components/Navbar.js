e42baf69ba1e332a285ca9dc4114bff9b3ddb1f4
tran <fidtran@gmail.com>
2020-08-11 21:38:43 -0400

    edit navbar based on user status

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,6 +14,8 @@ import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Badge from "@material-ui/core/Badge";
 import IconButton from "@material-ui/core/IconButton";
+import VpnKeyIcon from "@material-ui/icons/VpnKey";
+import PersonAddIcon from "@material-ui/icons/PersonAdd";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -29,6 +31,7 @@ const StyledBadge = withStyles((theme) => ({
 	},
 }))(Badge);
 export default function Navbar(props) {
+	const token = useSelector((state) => state.authentication.token);
 	const products = useSelector((state) => state.cartManagement.products);
 	const classes = useStyles();
 	const dispatch = useDispatch();
@@ -45,69 +48,148 @@ export default function Navbar(props) {
 	const onLogout = (e) => {
 		e.preventDefault();
 		dispatch(logout());
+		history.push("/");
+	};
+	const onLogin = (e) => {
+		e.preventDefault();
 		history.push("/login");
 	};
+	const onSignup = (e) => {
+		e.preventDefault();
+		history.push("/signup");
+	};
 	return (
-		<div className={classes.root} id="navbar">
-			<Grid container spacing={3}>
-				<Grid item xs>
-					<div className="navbar--logout-btn">
-						<ExitToAppIcon style={{ marginLeft: 20 }} onClick={onLogout} />{" "}
-						Logout
-					</div>
-				</Grid>
-				<Grid item xs={8} className="navbar-p2">
-					<div className="brandName">
-						<span className="brandName_text" onClick={(e) => onBackHomePage(e)}>
-							LaMode
-						</span>
-					</div>
-					<div className="navbar--categories">
-						<div>
-							<ModalWomenProduct />
-						</div>
-						<div>
-							<ModalMenProduct />
-						</div>
-						<div>
-							<ModalGirlsProduct />
-						</div>
-						<div>
-							<span
-								id="navbar__all-products"
-								onClick={(e) => onShowProducts(e)}
-							>
-								All Products
-							</span>
-						</div>
-					</div>
-				</Grid>
-				<Grid item xs className="navbar-p3">
-					<div className="navbar-icons">
-						<Link to="/" style={{ display: "flex", alignItems: "center" }}>
-							<AccountCircleIcon style={{ color: "white" }} />
-						</Link>
-						<Link
-							to="/favorite"
-							style={{ display: "flex", alignItems: "center" }}
-						>
-							<Badge color="secondary" variant="dot">
-								<FavoriteIcon style={{ color: "white" }} />
-							</Badge>
-						</Link>
-						<Link to="/cart">
-							<IconButton aria-label="cart">
-								<StyledBadge badgeContent={products.length} color="secondary">
-									<LocalMallIcon
-										style={{ color: "white" }}
-										className="navbar-bag-icon"
-									/>
-								</StyledBadge>
-							</IconButton>
-						</Link>
-					</div>
-				</Grid>
-			</Grid>
+		<div>
+			{token ? (
+				<div className={classes.root} id="navbar">
+					<Grid container spacing={3}>
+						<Grid item xs>
+							<div className="navbar--logout-btn" onClick={onLogout}>
+								<ExitToAppIcon style={{ marginLeft: 20 }} />
+								Logout
+							</div>
+						</Grid>
+						<Grid item xs={8} className="navbar-p2">
+							<div className="brandName">
+								<span
+									className="brandName_text"
+									onClick={(e) => onBackHomePage(e)}
+								>
+									LaMode
+								</span>
+							</div>
+							<div className="navbar--categories">
+								<div>
+									<ModalWomenProduct />
+								</div>
+								<div>
+									<ModalMenProduct />
+								</div>
+								<div>
+									<ModalGirlsProduct />
+								</div>
+								<div>
+									<span
+										id="navbar__all-products"
+										onClick={(e) => onShowProducts(e)}
+									>
+										All Products
+									</span>
+								</div>
+							</div>
+						</Grid>
+						<Grid item xs className="navbar-p3">
+							<div className="navbar-icons">
+								<Link to="/" style={{ display: "flex", alignItems: "center" }}>
+									<AccountCircleIcon style={{ color: "white" }} />
+								</Link>
+								<Link
+									to="/favorite"
+									style={{ display: "flex", alignItems: "center" }}
+								>
+									<Badge color="secondary" variant="dot">
+										<FavoriteIcon style={{ color: "white" }} />
+									</Badge>
+								</Link>
+								<Link to="/cart">
+									<IconButton aria-label="cart">
+										<StyledBadge
+											badgeContent={products.length}
+											color="secondary"
+										>
+											<LocalMallIcon
+												style={{ color: "white" }}
+												className="navbar-bag-icon"
+											/>
+										</StyledBadge>
+									</IconButton>
+								</Link>
+							</div>
+						</Grid>
+					</Grid>
+				</div>
+			) : (
+				<div className={classes.root} id="navbar">
+					<Grid container spacing={3}>
+						<Grid item xs>
+							<div className="navbar--login-btn" onClick={onLogin}>
+								<VpnKeyIcon style={{ marginLeft: 20 }} />
+								Login
+							</div>
+							<div className="navbar--signup-btn" onClick={onSignup}>
+								<PersonAddIcon style={{ marginLeft: 20 }} />
+								Signup
+							</div>
+						</Grid>
+						<Grid item xs={8} className="navbar-p2">
+							<div className="brandName">
+								<span
+									className="brandName_text"
+									onClick={(e) => onBackHomePage(e)}
+								>
+									LaMode
+								</span>
+							</div>
+							<div className="navbar--categories">
+								<div>
+									<ModalWomenProduct />
+								</div>
+								<div>
+									<ModalMenProduct />
+								</div>
+								<div>
+									<ModalGirlsProduct />
+								</div>
+								<div>
+									<span
+										id="navbar__all-products"
+										onClick={(e) => onShowProducts(e)}
+									>
+										All Products
+									</span>
+								</div>
+							</div>
+						</Grid>
+						<Grid item xs className="navbar-p3">
+							<div className="navbar-icons">
+								<Link to="/cart">
+									<IconButton aria-label="cart">
+										<StyledBadge
+											badgeContent={products.length}
+											color="secondary"
+										>
+											<LocalMallIcon
+												style={{ color: "white" }}
+												className="navbar-bag-icon"
+											/>
+										</StyledBadge>
+									</IconButton>
+								</Link>
+							</div>
+						</Grid>
+					</Grid>
+				</div>
+			)}
 		</div>
 	);
 }
