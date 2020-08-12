0ee958c72fcae1e60e6d96038d9ded00a339f98f
tran <fidtran@gmail.com>
2020-08-12 14:28:35 -0400

    redeploy

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -2,8 +2,8 @@ import { create } from "axios";
 import store from "../store/configureStore";
 
 const api = create({
-	// baseURL: "https://lamodebackend2.herokuapp.com",
-	baseURL: "http://localhost:8080",
+	baseURL: "https://lamodebackend2.herokuapp.com",
+	// baseURL: "http://localhost:8080",
 	headers: {
 		Accept: "application/json",
 		"Content-Type": "application/json",
